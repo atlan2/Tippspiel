afac655b7823c6e2b34ca7cce51fc85737abe21c
Matthias <matthias@mbeyerle.com>
2014-05-24 13:20:31 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Matthias on 24.05.14.
+ */
+(function() {
+    var app = angular.module('tippspiel', [])
+    app.controller('TippspielController', function() {
+
+    });
+
+    app.controller('TippspielController', function() {
+
+    });
+
+
+
+    var partien = [
+        {
+          mannschaft1 :  'Brasilien',
+          mannschaft2 :  'Kroatien',
+          datum: '2010/08/17 12:09:36',
+          ergebnis : ''
+        },
+        {
+            mannschaft1 :  'Mexiko',
+            mannschaft2 :  'Kamerun',
+            datum: '2010/08/17 22:09:36',
+            ergebnis : ''
+        }]
+})();
+
+
