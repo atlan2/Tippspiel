da8dd13e9aee416e1803c814e563d6e057aba849
Matthias <matthias@mbeyerle.com>
2014-07-17 23:45:39 +0200

    Spiele werden angezeigt

--- app.js
+++ app.js
@@ -2,19 +2,53 @@
  * Created by Matthias on 24.05.14.
  */
 (function () {
-    var app = angular.module('tippspiel',[]);
+    var app = angular.module('tippspiel', []);
 
-    app.controller('TippspielController', [ '$http', function ($http) {
-        this.init = function() {
-            $http.get('./data/mannschaften.json').then(function(mannschaft) {
-                console.log(""+mannschaft.id)
-            })
-        }
+    app.controller('TippspielController',function ($scope, $http) {
 
-        this.init();
-        this.partien = partien;
+        $scope.partien = [];
+        $scope.init = function () {
+            var mannschaften;
 
-        this.getPartie = function (partieId) {
+            $http.get('./data/mannschaften.json').then(function (mannschaftenJson) {
+                mannschaften = mannschaftenJson.data;
+                console.log("mannschaften geholt");
+                $http.get('./data/partien.json').then(function (partienJson) {
+                    console.log("partien geholt");
+                    var partien = partienJson.data;
+                    $scope.partien = partien;
+
+                    var mannschaftenDict = {};
+                    for(var i = 0; i< mannschaften.length; i++) {
+                        mannschaftenDict[mannschaften[i].id] = mannschaften[i];
+                    }
+
+                    var partienDict = {};
+                    for(var i = 0; i< partien.length; i++) {
+                        partienDict[partien[i].id] = partien[i];
+                        partien[i].mannschaft1 = mannschaftenDict[partien[i].mannschaft1];
+                        partien[i].mannschaft2 = mannschaftenDict[partien[i].mannschaft2];
+                    }
+
+                });
+
+
+
+//
+//                    for(var i = 0; i<partien.data.length; i++) {
+//                        var partie = partien.data[i];
+//                        console.log(mannschaft.id + " " + mannschaft.name );
+//                }
+
+//            })
+
+            });
+            console.log("init ferti");
+        };
+
+        $scope.init();
+
+        $scope.getPartie = function (partieId) {
             if (partieId == undefined) return null;
             var partie;
             angular.forEach(this.partien, function (value, key) {
@@ -25,9 +59,9 @@
             });
             return partie;
         };
-    }]);
+    });
 
-    app.controller('PanelController', function () {
+    app.controller('PanelController', function ($scope) {
         this.tab = 1;
         this.selectTab = function (setTab) {
             this.tab = setTab;
@@ -37,14 +71,14 @@
         }
     });
 
-    app.controller('TabController', function() {
+    app.controller('TabController', function () {
 
     });
-    app.controller('TippController', function () {
-        this.myTipps = meineTipps;
-        this.newTipp = [];
-        this.addTipp = function (tipp) {
-            myTipps.push(tipp);
+    app.controller('TippController', function ($scope) {
+        $scope.myTipps = meineTipps;
+        $scope.newTipp = [];
+        $scope.addTipp = function (tipp) {
+            $scope.myTipps.push(tipp);
         };
 
 
@@ -60,9 +94,7 @@
     ];
 
 
-    var partien =[];
 
-    var mannschaften =[];
 })();
 
 
